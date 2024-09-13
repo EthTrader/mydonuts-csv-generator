e2d2a9953ee76b06f5967f0b8262169318d73b47
reddito321 <150826989+reddito321@users.noreply.github.com>
2024-09-13 11:50:54 -0300

    Update csv_generator.py
    Limit of 50 comments per user per day is added, as well as the multiplier for "Link" posts.

--- csv_generator.py
+++ csv_generator.py
@@ -41,6 +41,7 @@ author: reddito321
   #%% Fetching only registered users
   
   wallets = pd.read_json("https://ethtrader.github.io/donut.distribution/users.json") 
+  wallets = wallets[wallets.address != '0xf8b45423AFb4144FCe5a2910fFE124067704043a']
   df_wallets = pd.DataFrame({'username': wallets['username'], 'blockchain_address': wallets['address']})
   
   registered_users = pd.merge(pd.DataFrame({'username':all_users}),df_wallets,how='inner',on=['username'])
@@ -49,20 +50,29 @@ author: reddito321
   #%% Comments
   cscores=np.zeros(len(users))
   clen=np.zeros(len(users))
-  
+
   for i in range(0,len(users)):
       cscore=0
+      dscore=0
+      climit = 50
       for j in range(0,len(comments)):
           c = pd.read_csv(comments[j])
+          d = pd.read_csv(daily[j])
+          
           idx = np.where(c['author']==users[i])[0]
-          cscore+=np.sum(c['score'][idx]-1)
-          clen[i]+=len(idx)
-      cscores[i]+=cscore    
+          idxd= np.where(d['author']==users[i])[0]
+          
+          d['score'] = d['score']/2
+          
+          user_comments = pd.concat([c.iloc[idx[0:]],d.iloc[idxd[0:]]]).sort_values('date')[0:climit]
+          cscore+=np.sum(user_comments['score']-1)
+          clen[i]+=len(user_comments)
+      cscores[i]+=cscore     
       
   #%% Posts
   pscores=np.zeros(len(users))
   plen=np.zeros(len(users))
-  
+
   for i in range(0,len(users)):
       # plen=0
       pscore=0
@@ -73,22 +83,14 @@ author: reddito321
                   plen[i]+=1
                   if p['flair'][k] == "Media" or p['flair'][k] == "Comedy" or p['flair'][k] == "Self Story":
                       pscore+=0.1*(p['score'][k]-1)
+                  elif p['flair'][k] == "OC - Original Content":
+                      pscore+=2*(p['score'][k]-1)
+                  elif p['flair'][k] == "Link":
+                      pscore+=0.75*(p['score'][k]-1)
                   else:
                       pscore+=p['score'][k]-1
-  
+
       pscores[i]+=pscore
-  
-  #%% Daily
-  
-  for i in range(0,len(users)):
-      dscore=0
-      for j in range(0,len(daily)):
-          d = pd.read_csv(daily[j])
-          idxd=np.where(d['author']==users[i])[0]
-          dscore+=np.sum(d['score'][idxd]-1)
-          clen[i]+=len(idxd)
-      cscores[i]+=dscore  
-  # 
   
   #%% pay2post
   
@@ -108,13 +110,13 @@ author: reddito321
   #%%
   
   userscores = pratio*pscores.clip(min=0) + cratio*cscores.clip(min=0)
-  
-  df = pd.DataFrame({'username': users, 'comments': clen.astype(int), 'comment_score':cscores*cratio, 'posts': plen.astype(int),'post_score': pscores*pratio})
+
+  df = pd.DataFrame({'username': users, 'comments': clen.astype(int), 'comment_upvotes':cscores,'comment_score':cscores*cratio, 'posts': plen.astype(int),'post_upvotes': pscores,'post_score': pscores*pratio})
   df = pd.merge(df,p2p_, how='left', on=['username']).fillna(0)
-  df['pay2post'] = -df['total_posts']*250
-  df['base'] = userscores+df['pay2post'] 
+  df['pay2post'] = -df['total_posts']*2.5*pratio
+  df['points'] = userscores+df['pay2post'] 
   #%%
-  final_csv = pd.merge(df[df['base']!=0],df_wallets, how='inner', on=['username']).sort_values(by=['base'],ascending=False).reset_index(drop=True)
+  final_csv = pd.merge(df[df['points']!=0],df_wallets, how='inner', on=['username']).sort_values(by=['points'],ascending=False).reset_index(drop=True)
   final_csv.to_csv(datadir+'/round_'+str(current_round)+'.csv',index=False)
-  
-  print(np.sum((userscores-plen*250))+250*np.sum(plen))
+
+  print(np.sum((userscores-plen*2.5*pratio))+2.5*pratio*np.sum(plen))
